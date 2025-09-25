b60619accee7fefca479d4e2c617d98168737fb4
Farhan <farhan24ti@mahasiswa.pcr.ac.id>
2025-09-25 08:50:25 +0700

    ini akun farhan

---
+++ resources/views/halaman-mahasiswa-detail.blade.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>SELAMAT DATANG</h1>
+        <h2>INI HALAMAN DETAIL MAHASISWA</h2>
+    </body>
+</html>
---
+++ resources/views/halaman-mahasiswa-profil.blade.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1>SELAMAT DATANG</h1>
+        <h2>INI HALAMAN PROFIL MAHASISWA</h2>
+    </body>
+</html>
